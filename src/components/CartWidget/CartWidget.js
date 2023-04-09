040fc833982c855ddd4b1217b920f5451572ac46
Santiago Arias <contcsantiagobaez@gmail.com>
2023-04-09 18:24:01 -0300

    GitHub Pages

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import cart from './assets/cart.svg'
+const CartWidget = () =>{
+
+    return (
+        <div>
+           <img src={cart} alt="cart-widget" style={{ height: '20px' }} />
+
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import CartWidget from "../CartWidget/CartWidget";
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>
+                Libreria
+            </h3>
+            <div>
+                <button>
+                    Inicio
+                
+
+                </button>
+                <button>
+                    Novedades
+
+                </button>
+                <button>
+                    Los mas vendidos
+
+                </button>
+            </div>
+            <CartWidget/>
+        </nav>
+    )
+}
+
+export default NavBar;
