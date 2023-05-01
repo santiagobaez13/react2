a135bbddde4dd4a72fbc486c2b2b68d8ac351819
Santiago Arias <contcsantiagobaez@gmail.com>
2023-05-01 19:39:48 -0300

    itemcount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
+
 
 function App() {
   return (
     <div className="App">
-     <NavBar />
-     <ItemListContainer greeting={'Bienvenido'}/>
+      <NavBar />
+      <ItemListContainer greeting={'Bienvenido'} />
+      <ItemCount>initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada  ', quantity)}</ItemCount>
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -26,3 +26,5 @@ const NavBar = () => {
 }
 
 export default NavBar;
+
+ 
