04515681f37c7747bce26d676de2c5763ed633db
santiagobaez13 <tu correo electrnico>
2023-05-07 23:11:56 -0300

    itemdetail

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemCount from './components/ItemCount/ItemCount';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <NavBar />
       <ItemListContainer greeting={'Bienvenido'} />
+      <ItemDetailContainer/>
       <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada  ', quantity)} />
     </div>
   );
